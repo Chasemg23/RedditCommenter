a417851b28c416e7827e62945de9e1a4f12e81da
Chase Gindlesperger <31860917+Chasemg23@users.noreply.github.com>
2018-10-28 19:37:26 -0400

    Beginning work

--- RedditCommenter.py
+++ RedditCommenter.py
@@ -1,1 +1,35 @@
+import praw
+import config
 import time
+
+def bot_login():
+	r = praw.Reddit(username = config.username,
+			password = config.password,
+			client_id = config.client_id,
+			client_secret = config.client_secret,
+			user_agent = "Testing new bot")
+	print "logged in!!!!"
+	return r
+
+def run_bot(r):
+	for submission in subreddit.new(limit=100):
+		#print "This is the original: " + submission.title + " posted by " + submission.author
+		search_for_post(submission)
+
+def search_for_post(submission):
+	for post in subreddit.search(submission.title, limit=100):
+		if post.title == submission.title:
+			if post.author != submission.author:
+				print "Found one"
+				time.sleep(5)
+				print submission.title + submission.author.name
+				print post.title + post.author.name
+			else:
+				print "Same person"
+
+
+r = bot_login()
+subreddit = r.subreddit('funny')
+while True:
+	run_bot(r)
+	time.sleep(5)
---
+++ config.py
@@ -0,0 +1,4 @@
+username = "TheFunnyManJan"
+password = "TheFunniestMan"
+client_id = "gAbYKtQjt7XltQ"
+client_secret = "Nr_XdWcfAn5MNKn7zySSKCYxUl8"
